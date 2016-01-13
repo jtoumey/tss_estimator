c85bef1a2b55ab780d48bdf69c54ff59cc9a3928
Julian Toumey <jtoumey@gmail.com>
2016-01-12 22:02:25 -0600

    convert_fit_file: debug print the bits of 1st record header

--- convert_fit_test/convert_fit_file.c
+++ convert_fit_test/convert_fit_file.c
@@ -4,12 +4,15 @@
 int convert_fit_file(char *ptr_file_name)
 {
 
-//   char buffer[30];
-//   unsigned int buffer;
-   unsigned char buffer; // 1 byte
+   unsigned char buffer; // 1 byte to store header length
    int bufferInt;
    FILE *ptr_myfile;
    int ii;
+
+   
+   unsigned char recordHeaderBuffer;
+   unsigned char recordHeaderMSB;
+   int recordHeader;
 
    unsigned char *bufferArray;   
    bufferArray = malloc(sizeof(unsigned char));
@@ -26,7 +29,7 @@ int convert_fit_file(char *ptr_file_name)
    // Read the file 
    // format is: (ptr to the memory in which to store, 
    //             size in bytes of each element to read,
-   //             number of elements each with size of the previous size,
+   //             number of elements to read (each with size of the previous size),
    //             pointer to a FILE that is the input stream)
    fread(&buffer,1,1,ptr_myfile);
 
@@ -37,7 +40,6 @@ int convert_fit_file(char *ptr_file_name)
    // (each unsigned char is 1 byte (= 8 bits) that can hold the remainder of the items 
    // in the header
    bufferInt = buffer;
-   printf("Here is the buffer as an int: %d\n",bufferInt);
    
    // Decrement buffer (we already read the first byte as the size of the header)
    bufferInt--;
@@ -51,6 +53,34 @@ int convert_fit_file(char *ptr_file_name)
    {
       printf("%2d byte of the header: %u\n",ii+1,bufferArray[ii]);
    }
+   // 
+
+   // Read the record header (1 byte)
+   fread(&recordHeaderBuffer,1,1,ptr_myfile);
+   // examine bits of this byte (jesus christ)
+   unsigned char mask = 1;
+   unsigned char bits[8];
+   for(ii = 0;ii < 8;ii++)
+   {
+      bits[ii] = (recordHeaderBuffer & (mask << ii)) != 0;    
+   }
+   // debug
+   for(ii = 0;ii < 8;ii++)
+   {
+      printf("bit: %d\n",bits[ii]);
+   }
+
+   //recordHeaderMSB = recordHeaderBuffer 
+
+
+
+   recordHeader = recordHeaderBuffer;
+   
+   printf("*** Here is the first record header buffer: one byte, unsigned char ***\n");
+   printf("%u\n",recordHeaderBuffer);
+   printf("*** Here is the first record header buffer, cast as integer ***\n");
+   printf("%2d\n",recordHeader);
+ 
 
    return(0);
 } 
